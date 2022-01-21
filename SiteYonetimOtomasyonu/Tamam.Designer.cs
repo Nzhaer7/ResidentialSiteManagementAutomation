1f7cd6f1dceb35fa1a5f4034ccf3a406ab7dd99f
Kamil <Kamil>
2022-01-21 22:20:33 +0300

    Yönetici Kaydol Tasarım Ve kodlaması

--- SiteYonetimOtomasyonu/Tamam.Designer.cs
+++ SiteYonetimOtomasyonu/Tamam.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(536, 286);
+            this.button1.Location = new System.Drawing.Point(557, 358);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 24);
@@ -46,7 +46,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(536, 152);
+            this.label1.Location = new System.Drawing.Point(557, 224);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 15);
             this.label1.TabIndex = 1;
@@ -54,7 +54,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(468, 215);
+            this.textBox1.Location = new System.Drawing.Point(489, 287);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(219, 23);
@@ -65,7 +65,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1124, 571);
+            this.ClientSize = new System.Drawing.Size(1284, 761);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
