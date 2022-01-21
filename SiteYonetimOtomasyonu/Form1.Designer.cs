ae4d285eca55081ca80b231962141bb3fe55934d
Süleyman OĞRAK <suleymanogrk24@gmail.com>
2022-01-21 17:57:56 +0300

    Kiraci giris tasarımı ve kodlaması

--- SiteYonetimOtomasyonu/Form1.Designer.cs
+++ SiteYonetimOtomasyonu/Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "KULLANICI";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // button3
             // 
