8072986059053939e9f669d113bc8e88c1d81f25
Süleyman OĞRAK <suleymanogrk24@gmail.com>
2022-01-21 23:53:42 +0300

    Temizlikçi arayüz tasarımı ve kodlaması

---
+++ SiteYonetimOtomasyonu/TemizlikciArayuz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiteYonetimOtomasyonu
+{
+    public partial class TemizlikciArayuz : Form
+    {
+        public TemizlikciArayuz()
+        {
+            InitializeComponent();
+        }
+
+        private void TemizlikciArayuz_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
