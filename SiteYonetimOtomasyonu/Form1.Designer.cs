c200f4be0c444c2817d7835c8e8a5be74507563e
Süleyman OĞRAK <suleymanogrk24@gmail.com>
2022-01-21 17:23:45 +0300

    Anasayfa tasarımı

--- SiteYonetimOtomasyonu/Form1.Designer.cs
+++ SiteYonetimOtomasyonu/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(15, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(414, 287);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "YÖNETİCİ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Location = new System.Drawing.Point(435, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(414, 287);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "KULLANICI";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(855, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(414, 287);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "PERSONEL";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1284, 761);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
